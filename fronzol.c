ec1903a35c3a7c5a4b44eef4c9e17ca0914816dd
noname <noname@dungeon.station>
2026-04-17 11:16:08 +0200

    add http message

--- fronzol.c
+++ fronzol.c
@@ -4,12 +4,29 @@
 
 const int MAX_URL_LENGTH = 2048;
 
-void getPage(char *url);
+int getPage(char *url);
 
+int getPage(char *url){
+    
+    char dataSend[MAX_URL_LENGTH+1000];  
+             
+    int sckConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sckConnect==-1){
+        printf("Socket error\n");
+        return -1;
+    }
+    //connect();
+    snprintf(dataSend, 
+            sizeof(dataSend), 
+            "GET /%s HTTP/1.1\r\n",
+            "Host: test\r\n",
+            "Connection: close\r\n"
+            "\r\n",
+            url);
 
-
-void getPage(char *url){
-    socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int sStatus = send(sckConnect, dataSend, sizeof(dataSend),0);
+    close(sckConnect);
+    return 0;
 }
 
 int main(int argc, char* argv[]){
@@ -18,7 +35,7 @@ int main(int argc, char* argv[]){
     
     printf("Url: ");
     fgets(url, MAX_URL_LENGTH-1, stdin);
-    printf("URL IS %s\n", url);
+    
 
     return 0;
 }
