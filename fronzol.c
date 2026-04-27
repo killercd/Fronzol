89860b5a0e257b0026a806065cac9e718ccfd6cc
noname <noname@dungeon.station>
2026-04-27 17:53:12 +0200

    add newline crop

--- fronzol.c
+++ fronzol.c
@@ -1,41 +1,81 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>  
+#include <arpa/inet.h>
+#include <string.h>
 
 const int MAX_URL_LENGTH = 2048;
 
-int getPage(char *url);
+
+void cropNewLine(char *str){
+    while(*str!='\x00'){
+        if(*str=='\x0D' || *str=='\x0A')
+        {
+            *str='\x00';
+            return;
+        }
+        str++;
+
+    }
+}
 
 int getPage(char *url){
     
+    
     char dataSend[MAX_URL_LENGTH+1000];  
-             
-    int sckConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    struct sockaddr_in server_addr;
+    int sckConnect;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(80);
+
+    
+    if (inet_pton(AF_INET, url, &server_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return 0;
+    }
+    sckConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
     if(sckConnect==-1){
         printf("Socket error\n");
-        return -1;
+        return 0;
     }
-    //connect();
+    if (connect(sckConnect, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connection error\n");
+        close(sckConnect);
+        return 0;
+    }
+    char page[]="robots.txt";
     snprintf(dataSend, 
             sizeof(dataSend), 
-            "GET /%s HTTP/1.1\r\n",
-            "Host: test\r\n",
+            "GET /%s HTTP/1.1\r\n"
+            "Host: test\r\n"
             "Connection: close\r\n"
             "\r\n",
-            url);
+            page);
 
-    int sStatus = send(sckConnect, dataSend, sizeof(dataSend),0);
+    int sStatus = send(sckConnect, dataSend, strlen(dataSend),0);
+    char retBuffer[1024];
+    memset(retBuffer, 0, sizeof(retBuffer));
+    int bRecv = recv(sckConnect,retBuffer, sizeof(retBuffer)-1, 0);
+    while(bRecv>0){
+        printf("%s", retBuffer);
+        bRecv = recv(sckConnect,retBuffer, sizeof(retBuffer)-1, 0);
+    }
     close(sckConnect);
-    return 0;
+    return 1;
 }
 
 int main(int argc, char* argv[]){
 
     char url[MAX_URL_LENGTH];
     
-    printf("Url: ");
+    printf("URL: ");
     fgets(url, MAX_URL_LENGTH-1, stdin);
-    
+    cropNewLine(url);
+    getPage(url);
 
     return 0;
 }
